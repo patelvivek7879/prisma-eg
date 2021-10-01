92e335f65fd75a6b63eb5a8d1a7e6a4a4b29e1cc
Vivek Patel <patelvivek7879@gmail.com>
2021-10-02 03:28:57 +0530

    react added

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+//const getEmployees = () => {
+  //const p = new Promise((resolve) => {
+    // fetch("/api/employee",{
+    // ).then((response) => {
+    //   return response.json();
+    // })
+    // .then((employees) => {
+    //  resolve(employees);
+    // })
+  //return p;
+//}
+
+const getEmployees = () =>{
+   return axios.get("/api/employee")
+    .then((response)=> {
+      return response.data;
+    })
+  }
+
+const App = () => {
+  const [employees, setEmployees] = React.useState([]);
+
+  React.useEffect(()=>{
+    const p = getEmployees();
+  p.then((employees) => {
+    console.log(employees);
+    setEmployees(employees);
+  })   
+  },[]);
+  return ( 
+  <div className = "App" >
+    <h1>Employees</h1>
+    <EmployeeListComponent employees={employees}/>
+  </div>
   );
+}
+
+const EmployeeListComponent = ({employees}) => {
+  return ( 
+    <React.Fragment >
+    <ul> 
+    {
+      employees.map((employee) => {
+        return(
+        <li key = {
+            employee.id
+          } > {
+            employee.name
+          } 
+          </li>
+        )
+      })
+    } 
+    </ul>
+    </React.Fragment>
+  )
 }
 
 export default App;
